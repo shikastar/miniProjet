da962afecc9005130ef9e3df434af5ceca8f376b
shikastar <achrafsettar7@gmail.com>
2025-09-02 17:16:53 +0100

    add option 3&4&5

--- miniprojet.c
+++ miniprojet.c
@@ -7,19 +7,20 @@
 #define MAX_BOOKS 10000
 
 struct Book {
-
     char title[MAX_SIZE];
     char author[MAX_SIZE];
     double price;
     int quantity;
-
 };
 
 struct Library {
     struct Book Books[MAX_BOOKS];
     int numberOfBooks;
+    int booksQuantity;
 };
 
+
+// Book functions 
 
 void    setTitle(struct Book *book) {
     printf("Enter Book title please:\n");
@@ -35,16 +36,33 @@ void    setPrice(struct Book *book) {
     scanf("%lf", &book->price);
 }
 void    setQuantity(struct Book *book) {
-    printf("Enter Book price please: \n");
+    printf("Enter Book quantity please: \n");
     scanf("%d", &book->quantity);
 }
 
 void    showInfos(struct Book *book) {
+    printf("Book info:\n");
     printf("    Title: %s\n", book->title);
     printf("    Author: %s\n", book->author);
     printf("    Price: %lf\n", book->price);
     printf("    Quantity: %d\n", book->quantity);
 }
+
+bool    isSameBookName(char *s, char *t) {
+    if (!s || !t) return 0;
+    int i = 0;
+    while (s[i] && t[i]) {
+        if (s[i] != t[i]) return 0;
+        i++;
+    }
+    return (s[i] == t[i]);
+}
+
+bool    isDeleted(struct Book *book) {
+    return (book->quantity == -1);
+}
+
+// library functions
 
 void    AddBook(struct Library *library) {
     struct Book newBook;
@@ -55,42 +73,101 @@ void    AddBook(struct Library *library) {
 
     library->Books[library->numberOfBooks] = newBook;
     library->numberOfBooks++;
+    library->booksQuantity += newBook.quantity;
 }
 
 void    showBooksInfo(struct Library *library) {
     printf("---------------------\n");
-    printf("there is %d books.\n", library->numberOfBooks);
+    printf("Here is all the books in library.\n");
     for (int i = 0; i < library->numberOfBooks; i++) {
-        printf("book number %d infos:\n", i);
+        if (isDeleted(&library->Books[i])) continue;
         showInfos(&library->Books[i]);
     }
     printf("---------------------\n");
 }
 
-void    DisplayMenu() {
-    printf("===============MENU=============================\n");
-    printf("1- Add a book.\n");
-    printf("2- Show all books informations.\n");
-    printf("Enter an option from the menu please:\n");
+
+
+void    changeBookQuantity(struct Library *library) {
+    printf("---------------------\n");
+    char title[MAX_SIZE];
+    int newQuantity;
+    
+    printf("Enter book title that you want to change it's quantity: \n");
+    scanf("%s", title);
+    printf("Enter new book quantity: \n");
+    scanf("%d", &newQuantity);
+    
+    for (int i = 0; i < library->numberOfBooks; i++) {
+        if (isDeleted(&library->Books[i])) continue;
+        if (isSameBookName(library->Books[i].title, title)) {
+            library->booksQuantity -= library->Books[i].quantity;
+            library->Books[i].quantity = newQuantity;
+            library->booksQuantity += library->Books[i].quantity;
+            break ;
+        }
+    }
 }
+
+void    deleteBook(struct Library *library) {
+    printf("Enter book title that you want to delete: \n");
+    char title[MAX_SIZE];
+    scanf("%s", title);
+
+    for (int i = 0; i < library->numberOfBooks; i++) {
+        if (isDeleted(&library->Books[i])) continue;
+        if (isSameBookName(library->Books[i].title, title)) {
+            library->booksQuantity -= library->Books[i].quantity;
+            library->Books[i].quantity = -1;
+            break;
+        }
+    }
+}
+
+void    showBooksQuantity(struct Library *library) {
+    printf("---------------------\n");
+    printf("total books quantities is : %d\n", library->booksQuantity);
+}
+
+
+// Menu functions 
 
 void    checkOption(struct Library *library, int option) {
     
     switch (option) {
         case 1:
-            AddBook(library);
-            break;
+        AddBook(library);
+        break;
         case 2 :
-        showBooksInfo(library);
+            showBooksInfo(library);
+            break;
+        case 3 :
+            changeBookQuantity(library);
+            break;
+        case 4 :
+            deleteBook(library);
+            break;
+        case 5 :
+            showBooksQuantity(library);
             break;
         default :
             printf("Enter a valid option please.\n");
     }
 }
+    
+void    DisplayMenu() {
+    printf("\n===============MENU=============================\n");
+    printf("1- Add a book.\n");
+    printf("2- Show all books informations.\n");
+    printf("3- Change book quantity.\n");
+    printf("4- Delete a book.\n");
+    printf("5- show quantitiy of all books.\n");
+    printf("Enter an option from the menu please:\n");
+}
 
 int main() {
-
     struct Library library;
+    library.booksQuantity = 0;
     library.numberOfBooks = 0;
     while (true) {
         DisplayMenu();
